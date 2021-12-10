48030271ba9126ba10bf5ca00345ce3a952b6580
Mark David <44349634+markd69@users.noreply.github.com>
2021-12-09 19:31:38 -0500

    Create multi_api.py

---
+++ functions/multi_api.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql_cred import *
+
+def get_multi_server(mc_daemon, mc_server):
+    get_server = "SELECT * FROM server WHERE daemon_id = %s AND id = %s"
+    mycursor.execute(get_server, (mc_daemon, mc_server))
+    result = mycursor.fetchall()
+    if mycursor.rowcount == 0 or mycursor.rowcount > 2:
+        exit("No server found")
+    return result
+
+def get_multi_email(mc_server):
+    get_user_id = "SELECT * FROM user_server WHERE server_id = %s"
+    mycursor.execute(get_user_id, (mc_server,))
+    result = mycursor.fetchall()
+    if mycursor.rowcount == 0 or mycursor.rowcount > 2:
+        exit("No user ID found from server ID")
+    user_id = result[0][0]
+
+    # get email address with user id
+    get_email = "SELECT * FROM user WHERE id = %s"
+    mycursor.execute(get_email, (user_id,))
+    result = mycursor.fetchall()
+    if mycursor.rowcount == 0 or mycursor.rowcount > 2:
+        exit("No email address from user ID")
+    user_email = result[0][3]
+    return user_email
