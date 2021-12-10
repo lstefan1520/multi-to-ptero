dff6e66a61c1d5635182ef1eb76c9a136547ff66
Mark David <44349634+markd69@users.noreply.github.com>
2021-12-09 19:26:44 -0500

    Define Multi & WHMCS DB info

--- database_creds.py
+++ database_creds.py
@@ -1,1 +1,21 @@
 # Database Creds
+import mysql.connector
+
+# Multicraft Cred
+mydb = mysql.connector.connect(
+  host="",
+  user="",
+  password="",
+  database=""
+)
+mycursor = mydb.cursor()
+
+# WHMCS Cred
+
+mydb_whmcs = mysql.connector.connect(
+  host="",
+  user="",
+  password="",
+  database=""
+)
+mycursor_whmcs = mydb_whmcs.cursor()
