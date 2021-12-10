eb8eeb3d369f4a5060e059a7a4ae109630397bab
Mark David <44349634+markd69@users.noreply.github.com>
2021-12-09 19:30:05 -0500

    Start of a fun project!

---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+import requests
+import mysql.connector
+from mysql_cred import *
+from functions.ptero_api import *
+from functions.whmcs_api import *
+from functions.multi_api import *
+
+# Hard set variables for Multicraft info. Will be changed to CLI soon
+mc_daemon = 
+mc_server = 
