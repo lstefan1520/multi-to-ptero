ed978e6f020f2ee03e9ce5b1587d16ebba1e3976
Mark David <44349634+markd69@users.noreply.github.com>
2021-12-17 19:45:21 -0500

    Complete create server function

--- functions/ptero_api.py
+++ functions/ptero_api.py
@@ -3,7 +3,7 @@ import json
 import random
 
 def con_create_allocation(apikey, nodeid, ip, port):
-    url = 'https://games.plox.host/api/application/nodes/{0}/allocations'.format(nodeid)
+    url = 'https://dev-ptero.ploxhost.com/api/application/nodes/{0}/allocations'.format(nodeid)
     headers = {
         "Authorization": "Bearer {0}".format(apikey),
         "Accept": "application/json",
@@ -18,7 +18,7 @@ def con_create_allocation(apikey, nodeid, ip, port):
         print("Pterodactyl allocation created:")
 
 def con_get_allocation_id(apikey, nodeid, ip, port):
-    url = 'https://games.plox.host/api/application/nodes/{0}/allocations'.format(nodeid)
+    url = 'https://dev-ptero.ploxhost.com/api/application/nodes/{0}/allocations'.format(nodeid)
     headers = {
         "Authorization": "Bearer {0}".format(apikey),
         "Accept": "application/json",
@@ -34,7 +34,7 @@ def con_get_allocation_id(apikey, nodeid, ip, port):
             return (i['attributes']['id'])
 
 def con_user_create(apikey, email, first_name, last_name):
-    url = 'https://games.plox.host/api/application/users'
+    url = 'https://dev-ptero.ploxhost.com/api/application/users'
     headers = {
         "Authorization": "Bearer {0}".format(apikey),
         "Accept": "application/json",
@@ -56,7 +56,7 @@ def con_user_create(apikey, email, first_name, last_name):
         print("Pterodactyl user account created")
 
 def con_get_user_id(apikey, email):
-    url = 'https://games.plox.host/api/application/users'
+    url = 'https://dev-ptero.ploxhost.com/api/application/users'
     headers = {
         "Authorization": "Bearer {0}".format(apikey),
         "Accept": "application/json",
@@ -74,7 +74,7 @@ def con_get_user_id(apikey, email):
 def con_create_server(apikey, name, user, nestid, egg, memory, swap, disk, io, cpu, databases, backups, allocations, allocation_id):
     print("\nDetecting docker image & startup command\n")
     # Retrieve egg information
-    url = 'https://games.plox.host/api/application/nests/{0}/eggs/{1}?include=variables'.format(nestid, egg)
+    url = 'https://dev-ptero.ploxhost.com/api/application/nests/{0}/eggs/{1}?include=variables'.format(nestid, egg)
     headers = {
         "Authorization": "Bearer {0}".format(apikey),
         "Accept": "application/json",
@@ -86,31 +86,29 @@ def con_create_server(apikey, name, user, nestid, egg, memory, swap, disk, io, c
     docker_image = data['attributes']['docker_image']
     startup_cmd = data['attributes']['startup']
 
-   # exit(data['attributes'])
     # Get startup variables
     # TODO: Change this to ask user to supply the startup parmas instead of using the default values
-    envdata = []
-
+    envdata = {}
     for i in data['attributes']['relationships']['variables']['data']:
         env = i['attributes']['env_variable']
         default = i['attributes']['default_value']
-        envdata.append(default)
-# TODO: Match the values of env data & default value
+        envdata[env] = default
 
-    url = 'https://games.plox.host/api/application/servers'
+    url = 'https://dev-ptero.ploxhost.com/api/application/servers'
     headers = {
         "Authorization": "Bearer {0}".format(apikey),
         "Accept": "application/json",
         "Content-Type": "application/json",
         "cookie": "pterodactyl_session=eyJpdiI6InhIVXp5ZE43WlMxUU1NQ1pyNWRFa1E9PSIsInZhbHVlIjoiQTNpcE9JV3FlcmZ6Ym9vS0dBTmxXMGtST2xyTFJvVEM5NWVWbVFJSnV6S1dwcTVGWHBhZzdjMHpkN0RNdDVkQiIsIm1hYyI6IjAxYTI5NDY1OWMzNDJlZWU2OTc3ZDYxYzIyMzlhZTFiYWY1ZjgwMjAwZjY3MDU4ZDYwMzhjOTRmYjMzNDliN2YifQ%253D%253D"
     }
-    payload = {'name': '{0}'.format(name), 'user': '{0}'.format(user), 'egg': '{0}'.format(egg), 'docker_image': '{0}'.format(docker_image), 'startup': '{0}'.format(startup_cmd), 'limits': {'memory': '{0}'.format(memory), 'swap': '{0}'.format(swap), 'disk': '{0}'.format(disk), 'io': '{0}'.format(io), 'cpu': '{0}'.format(cpu)}, 'feature_limits': {'databases': '{0}'.format(databases), 'backups': '{0}'.format(backups), 'allocations': '{0}'.format(allocations)}, 'allocations': {'default': '{0}'.format(allocation_id)} }
+    payload = {'name': '{0}'.format(name), 'user': '{0}'.format(user), 'egg': '{0}'.format(egg), 'docker_image': '{0}'.format(docker_image), 'startup': '{0}'.format(startup_cmd), 'environment': envdata, 'limits': {'memory': '{0}'.format(memory), 'swap': '{0}'.format(swap), 'disk': '{0}'.format(disk), 'io': '{0}'.format(io), 'cpu': '{0}'.format(cpu)}, 'feature_limits': {'databases': '{0}'.format(databases), 'backups': '{0}'.format(backups), 'allocations': '{0}'.format(allocations)}, 'allocation': {'default': '{0}'.format(allocation_id)} }
     response = requests.request('POST', url, json=payload, headers=headers)
+    data = json.loads(response.text)
 
-    print(response.text)
     if response.status_code != 201:
         exit("Error while creating server")
     else:
         print("Pterodactyl server created:")
+        print("\nID: " + data['attributes']['id'] + "\n")
 
 
